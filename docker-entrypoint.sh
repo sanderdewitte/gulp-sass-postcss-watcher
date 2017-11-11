ffafd9b15607d549a5d959cff32885ec1d943bd9
Sander de Witte <github@sanderdewitte.com>
2017-11-11 16:17:50 +0100

    Initial release of docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -e
+
+WATCH=0
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        watch)
+            WATCH=1
+            shift
+        -s|--src)
+            SOURCE="$2"
+            shift
+            shift
+            ;;
+        -d|--dest)
+            DESTINATION="$2"
+            shift
+            shift
+            ;;
+        *)
+            POSITIONAL+=("$1")
+            shift
+            ;;
+    esac
+done
+
+set -- "${POSITIONAL[@]}"
+
+cd $WORK_DIR
+if [ $WATCH ]; then
+    npm link gulp
+    gulp watch --src $SOURCE --dest $DESTINATION
+fi
